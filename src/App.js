23762d86f886f52d2fe87101704e8a22488ba697
Eduardo da Silva Peixinho <eduardopeixinho@gmail.com>
2025-03-13 22:10:01 -0300

    - Atualização do APP conforme novos componentes criados.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { Container, Button, Typography } from "@mui/material";
+import AgendaList from "./components/AgendaList";
+import AgendaForm from "./components/AgendaForm";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Container>
+        <Typography variant="h4" gutterBottom>
+          Gerenciamento de Eventos
+        </Typography>
+        <Button variant="contained" component={Link} to="/criar">
+          Criar Novo Evento
+        </Button>
+        <Routes>
+          <Route path="/" element={<AgendaList />} />
+          <Route path="/criar" element={<AgendaForm />} />
+          <Route path="/editar/:id" element={<AgendaForm />} />
+        </Routes>
+      </Container>
+    </Router>
   );
-}
+};
 
 export default App;
